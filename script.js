ad0aaeca040306e75c5d8e6d34085298612cb145
Jaylen Wilson <Jaylenaverywilson@gmail.com>
2021-09-16 18:02:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+//elements
+const img = document.getElementById('imgID');
+const cardInfo = document.getElementById('cardInfo');
+const cardContainer = document.getElementById('cardContainer');
+const searchResults = document.getElementById('searchResults')
+let url;
+
+
+
